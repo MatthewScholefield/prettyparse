18c57d71b58afd2d8d9dc605ade8f3ca6e66895d
Matthew Scholefield <matthew331199@gmail.com>
2018-07-10 14:38:56 -0500

    Increment version to 0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='prettyparse',
-    version='0.1.3',
+    version='0.1.4',
     packages=find_packages(),
     author='Matthew Scholefield',
     author_email='matthew331199@gmail.com',
