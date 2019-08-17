406a2cb5fd3bc16f0e164bf9dce50a4a61cf65c3
Matthew D. Scholefield <matthew331199@gmail.com>
2019-08-16 21:51:33 -0500

    Increment version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='prettyparse',
-    version='0.1.4',
+    version='0.2.0',
     packages=['prettyparse'],
     author='Matthew Scholefield',
     author_email='matthew331199@gmail.com',
